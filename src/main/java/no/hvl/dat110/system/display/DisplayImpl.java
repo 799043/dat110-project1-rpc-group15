dc18b748b2f8c38e84613d959f37665bfd816387
Younez <184885@stud.hvl.no>
2026-02-10 14:52:15 +0100

    Implementert DisplayImpl

--- src/main/java/no/hvl/dat110/system/display/DisplayImpl.java
+++ src/main/java/no/hvl/dat110/system/display/DisplayImpl.java
@@ -8,26 +8,27 @@ import no.hvl.dat110.rpc.RPCServer;
 public class DisplayImpl extends RPCRemoteImpl {
 
 	public DisplayImpl(byte rpcid, RPCServer rpcserver) {
-		super(rpcid,rpcserver);
+		super(rpcid, rpcserver);
 	}
 
 	public void write(String message) {
 		System.out.println("DISPLAY:" + message);
 	}
-	
+
 	public byte[] invoke(byte[] param) {
-		
+
 		byte[] returnval = null;
-		
-		// TODO - START: 
+
+		// TODO - START:
 		// implement unmarshalling, call, and marshall for write RPC method
 		// look at how this is done in the SensorImpl class for the read method
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
+
+		String s = RPCUtils.unmarshallString(param);
+		write(s);
+		returnval = RPCUtils.marshallVoid();
 		
 		// TODO - END
-		
+
 		return returnval;
 	}
 }
